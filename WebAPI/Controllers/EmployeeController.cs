daa34384af2c04e1db73a18cca17d9437eba5694
Karl Thomas <karlthomas23@gmail.com>
2022-05-17 21:11:29 +0930

    changed validation message

--- WebAPI/Controllers/EmployeeController.cs
+++ WebAPI/Controllers/EmployeeController.cs
@@ -32,7 +32,7 @@ namespace WebAPI.Controllers
             {
                 var validatorResult = _employeeValidator.IsValid(employee);
                 if (!validatorResult.IsValid)
-                    return UnprocessableEntity("Some specific error message from the validator object"); 
+                    return UnprocessableEntity(validatorResult.ErrorMessage); 
                     // They want 422 status for validation errors
 
                 return Ok(_powerShellOperations.ExecuteScript("GetEmployeePermisions.ps1", _employeeStringify.GetEmployee(employee, authorisation)));
