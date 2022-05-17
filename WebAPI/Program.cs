b58f0d395cd048b239434c99c8b0b7bbace7489f
Karl Thomas <karlthomas23@gmail.com>
2022-05-17 21:14:29 +0930

    Added more services

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Application.Validation;
+using Helper;
 using Infrastructure.Powershell;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IEmployeeValidator, EmployeeValidator>();
 builder.Services.AddScoped<IPowerShellOperations, PowerShellOperations>();
+builder.Services.AddScoped<IEmployeeStringify, EmployeeStringify>();
+
+// Ignore this crap
+builder.Services.AddScoped<Application2.Validation.IEmployeeValidator, Application2.Validation.EmployeeValidator>();
+builder.Services.AddScoped<Infrastructure2.Powershell.IPowerShellOperations, Infrastructure2.Powershell.PowerShellOperations>();
+builder.Services.AddScoped<Service.Helper.IEmployeeStringify, Service.Helper.EmployeeStringify>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
